dd5392e5978fb26961f637e034d6b4ff1da96bc8
Andy <Ishak@CLM-1YBHTDD.local>
2018-04-01 11:36:48 +0200

    CORS problem fixed. added server.js as backend

--- server.js
+++ server.js
@@ -1,10 +1,56 @@
 const express = require('express');
-
+const cors = require('cors');
+const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 5000;
+app.use(cors());
 
-app.get('./api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+var config = {
+  headers: {
+    'Accept':'application/json',
+    'Ocp-Apim-Subscription-Key':'db1281e5a3c744588d24b32202d6ba11',
+    'Access-Control-Allow-Origin': '*',
+  }
+};
+var options = {
+  dotfiles: 'ignore',
+  etag: false,
+  extensions: ['htm', 'html'],
+  index: false,
+  maxAge: '1d',
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now())
+  }
+}
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/swedavia', function (req, res, next) {
+  axios.get("https://api.swedavia.se/flightinfo/v2/ARN/departures/2018-04-04",config)
+  .then(function (res) {
+    data = res.data;
+    console.log(data);
+  })
+  .catch(function (error) {
+  console.log(error);
+  });
+  res.json(data);
+})
+
+
+
+
+app.options('/', cors())
+app.delete('/', cors(), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(3001, function () {
+  console.log('CORS-enabled web server listening on port 3001')
+})
--- src/store/appStore.js
+++ src/store/appStore.js
@@ -6,23 +6,10 @@ export class AppStore {
   @observable test = 1;
 
 
-  config = {
-    headers: {
-      'Accept':'application/json',
-      'Ocp-Apim-Subscription-Key':'db1281e5a3c744588d24b32202d6ba11',
-      'Access-Control-Allow-Origin': '*',
-    },
-    proxy: {
-      host: '96.37.92.170',
-      port: 8080
-    }
-  };
-
   @action
   getSwedaviaData() {
-  console.log(this.config);
-  console.log("running get Swedavia data");
-  axios.get("https://api.swedavia.se/flightinfo/v2/ARN/departures/2018-04-04",this.config)
+
+  axios.get("http://localhost:3001/swedavia")
   .then(function (response) {
   console.log('then');
   console.log(response);
